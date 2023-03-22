506086290a271bf7b86e2dd39b0e33cf0811199d
Neo-maru <123855072+Neo-maru@users.noreply.github.com>
2023-03-22 15:08:00 +0900

    Update webhook.php

--- webhook.php
+++ webhook.php
@@ -1,11 +1,11 @@
 <?php
 
-$message = ["text" => "Slack APIメッセージ投稿テストです。"];
+$message = ["text" => "Slack APIメッセージ投稿テストです。"]; //連想配列で入れる。変数はスラックで指定されているものを使う。
 
-$ch = curl_init("https://hooks.slack.com/services/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");
+$ch = curl_init("https://hooks.slack.com/services/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"); //webhookのurlをcurlで設定
 curl_setopt( $ch, CURLOPT_POSTFIELDS, json_encode($message));
 curl_setopt( $ch, CURLOPT_HTTPHEADER, array("Content-type: application/json"));
-curl_setopt( $ch, CURLOPT_RETURNTRANSFER, false );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, false ); //基本はtrueで
 curl_exec($ch);
 // error_log(curl_error($ch));
-curl_close($ch);
+curl_close($ch);　//セッションのクローズ忘れない
